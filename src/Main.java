29574abb824bac9105d6f6c64a12d9204ad178ad
jolenic <jennifer.olenic@gmail.com>
2020-09-18 15:58:47 -0400

    Allow user to choose how many cards to be pulled per question

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,25 @@ public class Main {
         Deck tarotDeck = new Deck();
         Scanner input = new Scanner(System.in);
         int nextCard = 1;
-        System.out.println("Welcome, let me read your cards...");
-        do {
-            tarotDeck.draw();
+        System.out.println("Welcome, welcome...");
+        int numCards = 1;
+
+        while (nextCard == 1) {
+            System.out.println("Please think of a question you would like to have answered.");
+            System.out.println("How many cards shall I pull for you?");
+            numCards = input.nextInt();
+            for (int k = 0; k < numCards; k++) {
+                tarotDeck.draw();
+                System.out.println();
+            }
             System.out.println("Hmm, interesting....");
-            System.out.println("Draw another card? (1 for yes, 2 for no)");
+            System.out.println("Another question? (1 for yes, 2 for no)");
             //technically anything other than 1 is a no but......
             nextCard = input.nextInt();
             //Should validate that it's an int, but that's a job for later.  For now we /trust/.
-        } while (nextCard == 1);
+        }
         input.close();
         System.out.println("Until next time...");
-    }
+    } //end Main
 
-}
+} //end Class
