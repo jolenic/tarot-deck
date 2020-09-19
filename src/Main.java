1d79e79a5deed85bc97c16f8b6b16cbdedc3f079
jolenic <jennifer.olenic@gmail.com>
2020-09-18 22:37:32 -0400

    Added the illusion of choice for reading or story prompts (reading works, story prompts Do Not)

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,58 @@ public class Main {
     public static void main(String[] args) {
         Deck tarotDeck = new Deck();
         Scanner input = new Scanner(System.in);
-        int nextCard = 1;
         System.out.println("Welcome, welcome...");
-        int numCards = 1;
-
-        while (nextCard == 1) {
-            System.out.println("Please think of a question you would like to have answered:");
-            input.nextLine();
-            System.out.println("How many cards shall I pull for you?");
-            numCards = input.nextInt();
-            for (int k = 0; k < numCards; k++) {
-                tarotDeck.draw();
-                System.out.println();
-            }
-            System.out.println("Hmm, interesting....");
-            System.out.println("Another question? (1 for yes, 2 for no)");
-            //technically anything other than 1 is a no but......
-            nextCard = input.nextInt();
-            //Should validate that it's an int, but that's a job for later.  For now we /trust/.
+        //ask if the user wants a reading or story prompts
+        System.out.println("Enter R for a reading or S for story prompts:");
+        String response = input.nextLine();
+        switch (response) {
+            case "R":
+            case "r":
+                int nextCard = 1;
+                int numCards = 1;
+                while (nextCard == 1) {
+                    System.out.println("Please think of a question you would like to have answered:");
+                    //on repeats it's not waiting for this and idk why......
+                    input.nextLine();
+                    System.out.println("How many cards shall I pull for you?");
+                    numCards = input.nextInt();
+                    for (int k = 0; k < numCards; k++) {
+                        tarotDeck.draw();
+                        System.out.println();
+                    }
+                    System.out.println("Hmm, interesting....");
+                    System.out.println("Another question? (1 for yes, 2 for no)");
+                    //technically anything other than 1 is a no but......
+                    nextCard = input.nextInt();
+                    //Should validate that it's an int, but that's a job for later.  For now we /trust/.
+                }
+                break;
+            case "S":
+            case "s":
+                System.out.println("Just kidding you can't actually do that.");
+               /* int next = -1;
+                String story = null;
+                System.out.println("Here's your starting prompt:");
+                
+                while (next != 0) {
+                    tarotDeck.draw();
+                    story += input.nextLine();
+                    do {
+                        System.out.println("Enter 1 for another prompt or 0 to end.");
+                        next = input.nextInt();
+                    } while ((next != 1) && (next != 0));
+                    if (next == 0) {
+                        System.out.println("Thanks for playing!  Here's your final story:");
+                        System.out.println(story);
+                    }
+                }
+                */
+                break;
+            default:
+                System.out.println("You're not great at following simple directions, are you?");
         }
+        
+        
         input.close();
         System.out.println("Until next time...");
     } //end Main
